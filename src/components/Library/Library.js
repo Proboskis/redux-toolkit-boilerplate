dbe18aa62f9bbaf928370555ff6305576b35c538
Proboskis <nikolasurla22@gmail.com>
2022-11-10 13:20:43 +0100

    1. Initial commit.

---
+++ src/components/Library/Library.js
@@ -0,0 +1,33 @@
+const Library = () => {
+    return (
+        <div className='library'>
+            <h2>Library</h2>
+            <table>
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Rating</th>
+                    <th>&nbsp;</th>
+                </tr>
+                <tr>
+                    <td>Dune</td>
+                    <td>F. Herbert</td>
+                    <td>10</td>
+                    <td>
+                        <button className='delete'>x</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>The Return of The King</td>
+                    <td>J.R.R. Tolkien</td>
+                    <td>10</td>
+                    <td>
+                        <button className='delete'>x</button>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    );
+};
+
+export default Library;
