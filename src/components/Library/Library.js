456db30caa21d35e97dee5c5ea60d92ce6a6d9d1
Proboskis <nikolasurla22@gmail.com>
2022-11-10 13:53:16 +0100

    3. added actions, with redux toolkit and the testing was done in the fake reducer. Also added thead and tbody to fix the error.

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -3,28 +3,32 @@ const Library = () => {
         <div className='library'>
             <h2>Library</h2>
             <table>
-                <tr>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Rating</th>
-                    <th>&nbsp;</th>
-                </tr>
-                <tr>
-                    <td>Dune</td>
-                    <td>F. Herbert</td>
-                    <td>10</td>
-                    <td>
-                        <button className='delete'>x</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>The Return of The King</td>
-                    <td>J.R.R. Tolkien</td>
-                    <td>10</td>
-                    <td>
-                        <button className='delete'>x</button>
-                    </td>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Rating</th>
+                        <th>&nbsp;</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Dune</td>
+                        <td>F. Herbert</td>
+                        <td>10</td>
+                        <td>
+                            <button className='delete'>x</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>The Return of The King</td>
+                        <td>J.R.R. Tolkien</td>
+                        <td>10</td>
+                        <td>
+                            <button className='delete'>x</button>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     );
--- src/store/fakeReducer.js
+++ src/store/fakeReducer.js
@@ -1,5 +1,13 @@
 // this shall be deleted when the real reducers are created
+import {addBook} from "./action/bookActions";
+
 const initialState = { test: 'It works' };
+
+console.log(addBook.toString());
+console.log(`Action type: ${addBook}`);
+
+console.log(addBook());
+console.log(addBook('Book'));
 
 export default (state = initialState, action) => {
     switch (action.type) {
